cfe322a02658cda2463400ebbef1452378210d9e
Keijur0 <tran.qlam@gmail.com>
2024-07-25 15:31:31 +0200

    [Back] TopicController updates + javadoc

--- back/src/main/java/com/openclassrooms/mddapi/controller/TopicController.java
+++ back/src/main/java/com/openclassrooms/mddapi/controller/TopicController.java
@@ -2,6 +2,7 @@ package com.openclassrooms.mddapi.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,16 +10,24 @@ import org.springframework.web.bind.annotation.RestController;
 import com.openclassrooms.mddapi.model.Topic;
 import com.openclassrooms.mddapi.service.ITopicService;
 
+/**
+ * Controller for managing topics.
+ * This controller provides endpoints to handle requests related to {@link Topic} entities.
+ * 
+ * @author 
+ */
 @RestController
-@RequestMapping("/topic")
+@RequestMapping("/api/topic")
 public class TopicController {
 	
+	@Autowired
 	private ITopicService topicService;
-	
-	public TopicController(ITopicService topicService) {
-		this.topicService = topicService;		
-	}
 
+	/**
+     * Retrieves a list of all topics.
+     * 
+     * @return a list of {@link Topic} objects
+     */
 	@GetMapping
 	public List<Topic> getTopics() {
 		return topicService.getTopics();
