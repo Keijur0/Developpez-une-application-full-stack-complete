43220a3233b007badb033e63011049f7f586eecd
Keijur0 <tran.qlam@gmail.com>
2024-07-27 19:25:28 +0200

    [Front] JwtInterceptor, Auth/Unauth Guards

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { JwtInterceptor } from './interceptors/jwt.interceptor';
 import { HomeComponent } from './pages/home/home.component';
 import { TopicComponent } from './pages/topic/topic.component';
 
@@ -26,7 +27,9 @@ import { TopicComponent } from './pages/topic/topic.component';
     MatIconModule,
     MatCardModule
   ],
-  providers: [],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true }
+    ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
