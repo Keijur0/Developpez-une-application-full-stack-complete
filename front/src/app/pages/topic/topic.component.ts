8b9056e86c6d26bab0e13cef291831bc0aeb3ad1
Keijur0 <tran.qlam@gmail.com>
2024-07-29 12:29:57 +0200

    [Front] TopicComponent

--- front/src/app/pages/topic/topic.component.ts
+++ front/src/app/pages/topic/topic.component.ts
@@ -1,6 +1,7 @@
-import { Component } from "@angular/core";
-import { Observable } from "rxjs";
+import { Component, OnInit } from "@angular/core";
+import { map, Observable } from "rxjs";
 import { Topic } from "src/app/interfaces/topic.interface";
+import { User } from "src/app/interfaces/user.interface";
 import { SessionService } from "src/app/services/session.service";
 import { TopicService } from "src/app/services/topic.service";
 import { UserService } from "src/app/services/user.service";
@@ -10,19 +11,41 @@ import { UserService } from "src/app/services/user.service";
   templateUrl: './topic.component.html',
   styleUrls: ['./topic.component.scss']
 })
-export class TopicComponent {
+export class TopicComponent implements OnInit {
+
+  public user: User | undefined;
+  public userId: number;
+  public topics$: Observable<Topic[]>;
+  public subscriptions$: Observable<number[]>;
 
   constructor(
     private topicService: TopicService,
     private sessionService: SessionService,
     private userService: UserService
-  ) {}
+  ) {
+    this.userId = this.sessionService.sessionInfo!.id;
+    this.topics$ = this.topicService.getTopics();
+    this.subscriptions$ = this.userService.subscriptions$;
+  }
 
-  public topics$: Observable<Topic[]> = this.topicService.getTopics();
+  public ngOnInit(): void {
+    this.fetchUser();
+  }
 
-  public subscribe() {
+  private fetchUser(): void {
+    this.userService.getUser(this.userId).subscribe((user: User) => {
+      this.user = user;
+    });
+  }
 
+  public isSubscribed$(topicId: number): Observable<boolean> {
+    return this.subscriptions$.pipe(
+      map((subscriptions: number[]) => subscriptions.includes(topicId))
+    );
+  }
 
+  public subscribe(topicId: number): void {
+    this.userService.subscribe(this.userId, topicId).subscribe(_ => this.fetchUser());
   }
 
 }
